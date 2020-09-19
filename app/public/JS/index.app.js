20404ad90ebd5b8de1385a3a68ecb21b5e4966e8
prestonflamion <70488049+prestonflamion@users.noreply.github.com>
2020-09-18 21:26:00 -0400

    Updating JS files

---
+++ app/public/JS/index.app.js
@@ -0,0 +1,10 @@
+
+  var app = new Vue({
+      el: '#userProfile',
+      data: {"patientGuid": "SOME-REALLY-LONG-1234",
+      "firstName": "Sylvia",
+      "lastName": "Hernandez",
+      "dob": "2012-09-01",
+      "sexAtBirth": "F",
+      "priority": "critical"}
+    });
