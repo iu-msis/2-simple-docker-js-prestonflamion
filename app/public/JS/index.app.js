fd5fb4ee0766908bf204495623655c56d83a52da
prestonflamion <70488049+prestonflamion@users.noreply.github.com>
2020-09-18 21:29:00 -0400

    Preparing vue

--- app/public/JS/index.app.js
+++ app/public/JS/index.app.js
@@ -6,5 +6,10 @@
       "lastName": "Hernandez",
       "dob": "2012-09-01",
       "sexAtBirth": "F",
-      "priority": "critical"}
-    });
+      "priority": "critical",
+  },
+  created() {
+  console.log('this vue app has been created!');
+},
+methods: {}
+  });
