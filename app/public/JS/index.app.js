c1e817eaa7b6f3259cf2aa6f1b32390ff0b36f95
prestonflamion <70488049+prestonflamion@users.noreply.github.com>
2020-09-18 22:25:36 -0400

    Adding objects to vue object

--- app/public/JS/index.app.js
+++ app/public/JS/index.app.js
@@ -2,27 +2,34 @@
   var app = new Vue({
       el: '#userProfile',
       data: {
-      "patientGuid": "SOME-REALLY-LONG-1234",
-      "firstName": "Sylvia",
-      "lastName": "Hernandez",
-      "dob": "2012-09-01",
-      "sexAtBirth": "F",
-      "priority": "critical",
-  },
-  created() {
-  console.log('this vue app has been created!');
+      userName: "John",
+      userEmail: "ww@gmail.com",
+      userImgLarge:'',
+      userImgThumb:'',
+      userAge: '',
+      userCountry: '',
+      userBirthday:''
+      },
+      created() {
+        this.fetchUser();
 },
 
-methods: {
-  fetchUser: function() {
-    fetch('https:/randomuser.me/api/')
-    .then(response => response.json())
-    .then(data => {
-      var userData = data.results[0];
-      console.log(userData);
-      this.userName = userData.name.first + " " + userData.name.last;
+  methods: {
+    fetchUser: function() {
+      fetch('https:/randomuser.me/api/')
+      .then(response => response.json())
+      .then(data => {
+        var userData = data.results[0];
+        console.log(userData);
+        this.userName = userData.name.first + " " + userData.name.last;
+        this.userEmail = userData.email;
+        this.userImgLarge = userData.picture.large;
+        this.userImgThumb = userData.picture.thumbnail;
+        this.usarAge = userData.dob.age;
+        this.userCountry = userData.location.country;
+        this.userBirthday = userData.dob.date[5] + userData.dob.date[6] + userData.dob.date[4] + userData.dob.date[8] +userData.dob.date[9];
 
-    });
+      });
+    }
   }
-}
 })
