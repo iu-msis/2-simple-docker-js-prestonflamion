ace5d06f50569beb3d88066f9e932672bb372580
prestonflamion <70488049+prestonflamion@users.noreply.github.com>
2020-09-18 21:50:09 -0400

    Adding fetch

--- app/public/JS/index.app.js
+++ app/public/JS/index.app.js
@@ -1,7 +1,8 @@
 
   var app = new Vue({
       el: '#userProfile',
-      data: {"patientGuid": "SOME-REALLY-LONG-1234",
+      data: {
+      "patientGuid": "SOME-REALLY-LONG-1234",
       "firstName": "Sylvia",
       "lastName": "Hernandez",
       "dob": "2012-09-01",
@@ -11,5 +12,17 @@
   created() {
   console.log('this vue app has been created!');
 },
-methods: {}
-  });
+
+methods: {
+  fetchUser: function() {
+    fetch('https:/randomuser.me/api/')
+    .then(response => response.json())
+    .then(data => {
+      var userData = data.results[0];
+      console.log(userData);
+      this.userName = userData.name.first + " " + userData.name.last;
+
+    });
+  }
+}
+})
